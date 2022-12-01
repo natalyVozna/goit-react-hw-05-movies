5894554461819a7ec1fdcc0e99bd2a6e3b77e16b
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2022-12-01 10:06:35 +0200

    fix

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -4,19 +4,16 @@ export const useFetch = callback => {
   const [isError, setIsError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-  const fetchData = useCallback(
-    async params => {
-      try {
-        setIsLoading(true);
-        await callback(params);
-      } catch (error) {
-        setIsError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    },
-    [callback]
-  );
+  const fetchData = useCallback(async params => {
+    try {
+      setIsLoading(true);
+      await callback(params);
+    } catch (error) {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  });
 
   return [fetchData, isLoading, isError];
 };
